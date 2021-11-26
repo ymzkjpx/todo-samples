e27f1fda79187e4b44fadb0d0b55fedf39cbfe10
ymzkjpx <ymzkjpx@gmail.com>
2021-11-26 23:35:42 +0900

    add TopController

---
+++ src/main/java/com/todo/presentation/web/TopController.java
@@ -0,0 +1,13 @@
+package com.todo.presentation.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/")
+public class TopController {
+
+    @GetMapping("")
+    String index(){
+        return "top";
+    }
+}
