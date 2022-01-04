b881786988f148028dbb6d1320556a02261fb504
ymzkjpx <ymzkjpx@gmail.com>
2022-01-04 21:41:05 +0900

    BookeRegisterServiceを作成

---
+++ src/main/java/com/todo/application/BookRegisterService.java
@@ -0,0 +1,16 @@
+package com.todo.application;
+
+import com.todo.domain.model.book.Book;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookRegisterService {
+
+    /**
+     * 書籍を登録する
+     */
+    public void registerBook(Book book){
+        return;
+    }
+}
